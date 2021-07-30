f5952dc3fdf4af340e599de2dc0e05cc0de4493f
kckang1103 <kang377@purdue.edu>
2021-07-29 21:30:18 -0400

    changed menubar type to tabular

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -13,14 +13,14 @@ function MenuBar() {
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   const menuBar = user ? (
-    <Menu pointing secondary size="massive" color="teal">
+    <Menu tabular size="massive" color="teal">
       <Menu.Item name={user.username} active as={Link} to="/" />
       <Menu.Menu position="right">
         <Menu.Item name="logout" onClick={logout} />
       </Menu.Menu>
     </Menu>
   ) : (
-    <Menu pointing secondary size="massive" color="teal">
+    <Menu tabular size="massive" color="teal">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
